41073c2d79fca06b46c35ccf020e21d3be3cef5a
rebot333 <47825189+rebot333@users.noreply.github.com>
2019-05-15 12:05:24 -0600

    Update main.js

--- webdaw/main.js
+++ webdaw/main.js
@@ -5,19 +5,21 @@ var measures = 4;
 var time = 4;
 var noteColor = "rgb(135, 232, 111)";
 
-for (var i = 0; i < notes; i++) {
-  boxHTML += "<tr><th class='noteVal'>" + (i + 1) + "</th>";
-  for (var l = 0; l < time*measures; l++) {
-    if (l % time === 0) {
-      boxHTML += "<th class='boxes measureCell' onmousedown='switchCell(" + i + ", " + l + ")' id='cell_" + (i + 1) + "_" + (l + 1) + "'></th>";
-    } else {
-      boxHTML += "<th class='boxes' onmousedown='switchCell(" + i + ", " + l + ")' id='cell_" + (i + 1) + "_" + (l + 1) + "'></th>";
+function setupMidiEditor() {
+  for (var i = 0; i < notes; i++) {
+    boxHTML += "<tr><th class='noteVal'>" + (i + 1) + "</th>";
+    for (var l = 0; l < time*measures; l++) {
+      if (l % time === 0) {
+        boxHTML += "<th class='boxes measureCell' onmousedown='switchCell(" + i + ", " + l + ")' id='cell_" + (i + 1) + "_" + (l + 1) + "'></th>";
+      } else {
+        boxHTML += "<th class='boxes' onmousedown='switchCell(" + i + ", " + l + ")' id='cell_" + (i + 1) + "_" + (l + 1) + "'></th>";
+      }
     }
+    boxHTML += "</tr>";
   }
-  boxHTML += "</tr>";
+  boxHTML += endBoxHTML;
+  document.getElementById("noteArea").innerHTML = boxHTML;
 }
-boxHTML += endBoxHTML;
-document.getElementById("noteArea").innerHTML = boxHTML;
 
 
 function switchCell(a, b) {
