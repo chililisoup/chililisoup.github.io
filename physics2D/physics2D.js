730e4b2bf0aafeac6db102e583d07fc639bd292f
rebot333 <47825189+rebot333@users.noreply.github.com>
2019-09-20 12:52:36 -0600

    Add files via upload

--- physics2D/physics2D.js
+++ physics2D/physics2D.js
@@ -7,7 +7,7 @@ var Engine = Matter.Engine,
 // create an engine
 var engine = Engine.create();
 
-var canWidth = 800,
+var canWidth = 1000,
     canHeight = 600;
 
 // create a renderer
@@ -46,29 +46,30 @@ var render = Render.create({
 // create ground & walls
 var ground = Bodies.rectangle((canWidth * 0.5), (canHeight + 10), (canWidth + 10), 60, { isStatic: true }),
     wallA = Bodies.rectangle(0, ((canHeight * 0.5) + 5), 60, (canHeight + 10), { isStatic: true }),
-    wallB = Bodies.rectangle(canWidth, (canHeight * 0.5), 60, (canHeight + 10), { isStatic: true });
+    wallB = Bodies.rectangle(canWidth, (canHeight * 0.5), 60, (canHeight + 10), { isStatic: true }),
+    roof = Bodies.rectangle((canWidth * 0.5), 0, (canWidth + 10), 60, { isStatic: true });
 
 // add all of the bodies to the world
-World.add(engine.world, [ground, wallA, wallB]);
+World.add(engine.world, [ground, wallA, wallB, roof]);
   
 function spawnCircle() {
-    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 0, 50)]);
+    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 0, 50, { restitution: 0.4 })]);
 }
 
 function spawnTri() {
-    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 3, 50)]);
+    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 3, 50, { restitution: 0.4, angle: 1.564 })]);
 }
 
 function spawnQuad() {
-    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 4, 50)]);
+    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 4, 50, { restitution: 0.4 })]);
 }
 
 function spawnPent() {
-    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 5, 50)]);
+    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 5, 50, { restitution: 0.4, angle: 1.568 })]);
 }
 
 function spawnHex() {
-    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 6, 50)]);
+    World.add(engine.world, [Bodies.polygon((canWidth * 0.5), (canHeight * 0.5), 6, 50, { restitution: 0.4 })]);
 }
 
 // run the engine
